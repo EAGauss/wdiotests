402033de93bab561cc605d5f3bb8cd5eb63ca718
Emil Akhadov <mr.eagauss@gmail.com>
2021-03-20 10:11:40 +0200

    Update example.e2e.js

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -5,7 +5,7 @@ describe('Auth', () => {
     it('user logs in with valid data', () => {
         LoginPage.open();
         LoginPage.setLogin('testemilskyeng@gmail.com');
-        LoginPage.setPassword('test_M0Z1LL4-17');
+        LoginPage.setPassword('test_123456');
         LoginPage.clickSubmitButton();
         ProfilePage.isOpen();
     });
