79bf461c8907fa8ff59fc6575a8ad3000c97c767
Emil Akhadov <mr.eagauss@gmail.com>
2021-03-20 06:48:46 +0200

    Localcoding login page autotest added

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,14 +1,13 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    it('user logs in with valid data', () => {
         LoginPage.open();
-
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
+        LoginPage.setLogin('testemilskyeng@gmail.com');
+        LoginPage.setPassword('test_M0Z1LL4-17');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
     });
 });
 
